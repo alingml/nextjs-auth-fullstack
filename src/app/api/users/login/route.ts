6737dffd324c86f94a2315fea8b4e0f13aba57dc
Alin Gamalan <alin.gamalan@gmail.com>
2023-08-11 10:20:43 +0300

    signup backend route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,42 @@
+import connect from '@/app/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export default async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const {email, password} = reqBody;
+        console.log(reqBody);
+
+
+        //check if user exists
+        const user = await User.findOne({email});
+        if(!user) {
+            return NextResponse.json({error:"User does not exist"}, {status: 400});
+        }
+
+        //check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password);
+        if(!validPassword){
+            return NextResponse.json({error: "Invalid password"}, {status: 400})
+        }
+
+        //create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        //create token
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {expiresIn: "1h"})
+
+
+    } catch(err:any) {
+        return NextResponse.json({err: err.message}, {status: 500})
+    }
+}
